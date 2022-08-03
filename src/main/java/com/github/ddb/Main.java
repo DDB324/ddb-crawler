1806be98e89e7209625ac80eeee42bb825b893a7
DDB324 <jiangdaoran@163.com>
2022-08-04 02:13:31 +0800

    将多次的http请求放到一个循环中解决

--- src/main/java/com/github/ddb/Main.java
+++ src/main/java/com/github/ddb/Main.java
@@ -22,38 +22,43 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         List<String> linkPool = new ArrayList<>();
+        Set<String> processedPool = new HashSet<>();
         CloseableHttpClient httpclient = HttpClients.createDefault();
-        HttpGet httpGet = new HttpGet("https://sina.cn");
-        try (CloseableHttpResponse response = httpclient.execute(httpGet)) {
-            HttpEntity entity = response.getEntity();
-            InputStream content = entity.getContent();
-            String result = IOUtils.toString(content, StandardCharsets.UTF_8);
-            Document doc = Jsoup.parse(result);
-            Elements aTags = doc.select("a");
-            for (Element aTag :
-                    aTags) {
-                String href = aTag.attr("href");
-                if(href.contains("sina.cn")){
-                    linkPool.add(href);
-                }
-            }
-            EntityUtils.consume(entity);
-        }
+        linkPool.add("https://sina.cn");
+        while (!linkPool.isEmpty()) {
 
-        for (String link :
-                linkPool) {
-            CloseableHttpClient httpclient1 = HttpClients.createDefault();
-            HttpGet httpGet1 = new HttpGet(link);
+            //从链接池中取出一个链接
+            String link = linkPool.remove(linkPool.size() - 1);
+
+            //判断这个链接是否处理过，如果处理过就进入下一次循环，没有处理就将它放到处理池
+            if (processedPool.contains(link)) continue;
+            processedPool.add(link);
+
+            //构造请求并发送
+            HttpGet httpGet = new HttpGet(link);
+            //忽略cookie
             RequestConfig defaultConfig = RequestConfig.custom().setCookieSpec(CookieSpecs.IGNORE_COOKIES).build();
-            httpGet1.setConfig(defaultConfig);
-            try (CloseableHttpResponse response = httpclient1.execute(httpGet1)) {
+            httpGet.setConfig(defaultConfig);
+            try (CloseableHttpResponse response = httpclient.execute(httpGet)) {
                 HttpEntity entity = response.getEntity();
                 InputStream content = entity.getContent();
                 String result = IOUtils.toString(content, StandardCharsets.UTF_8);
                 Document doc = Jsoup.parse(result);
-                Elements article = doc.select("article");
-                Elements h1 = article.select("h1");
-                if(!Objects.equals(String.valueOf(h1), "")) {
+                //获取页面中的所有a标签，将符合要求的加入链接池。要做个限制，不然没完没了了。
+                if(linkPool.size()==0){
+                    Elements aTags = doc.select("a");
+                    for (Element aTag :
+                            aTags) {
+                        String href = aTag.attr("href");
+                        if (href.contains("sina.cn") && !href.contains("blog") && href.startsWith("https")) {
+                            linkPool.add(href);
+                        }
+                    }
+                }
+                //获取页面的article标签的h1标签的内容
+                Elements articleTag = doc.select("article");
+                if(!articleTag.isEmpty()){
+                    Elements h1 = articleTag.select("h1");
                     System.out.println(link);
                     System.out.println(h1.text());
                     System.out.println("----");
